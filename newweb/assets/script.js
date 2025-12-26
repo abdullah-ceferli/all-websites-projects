53b1daca8890449298f27f1195e645d57cd8b640
AbdullahCeferli <abdullahdj234@gmail.com>
2025-12-26 23:39:04 +0400

    Upload Backend web

---
+++ newweb/assets/script.js
@@ -0,0 +1,10 @@
+const headerSticky = document.querySelector(".header-sticky")
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        headerSticky.classList.add("background-header")
+    } 
+    else {
+        headerSticky.classList.remove("background-header")
+    }
+})
