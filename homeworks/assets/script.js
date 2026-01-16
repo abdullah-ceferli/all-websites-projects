93c2c9d9f3614f5a8218ec9c69e4923d82fd93af
AbdullahCeferli <abdullahdj234@gmail.com>
2026-01-16 23:16:58 +0400

    Upload homework

---
+++ homeworks/assets/script.js
@@ -0,0 +1,79 @@
+// 1. 1-dən 10-a qədər ədədləri çap et
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i)
+// }
+
+// ------------------------------------------------------
+
+// let count = 1
+
+// while (count < 11) {
+//     console.log(count)
+//     count++
+// }
+
+// 2. 10-dan 1-ə qədər geriyə say
+
+// for (let i = 11; i > 1; i--) {
+//     console.log(i)
+// }
+
+// ------------------------------------------------------
+
+// let count = 10
+
+// while (count > 0) {
+//     console.log(count)
+//     count--
+// }
+
+// 3. 1-dən 5-ə qədər ədədlərin kvadratını çap et
+
+// for (let i = 1; i < 6; i++) {
+//     console.log(i**2)
+// }
+
+// ------------------------------------------------------
+
+// let count = 1
+// let number = 1
+
+// while (count < 6) {
+//     console.log(number**2)
+//     number++
+//     count++
+// }
+
+// 4. 5 dəfə “Hello” yaz
+
+// for (let i = 1; i < 6; i++) {
+//     console.log("Hello", i)
+// }
+
+// ------------------------------------------------------
+
+// let count = 1
+
+// while (count < 6) {
+//     console.log("Hello", count)
+//     count++
+// }
+
+// 5. Arrayda olan datani cixart let data = [1, 2, 3, 4, 5, 6, 7]
+
+// let data = [1, 2, 3, 4, 5, 6, 7]
+
+// for (let i = 0; i < data.length; i++) {
+//     console.log(data[i])
+// }
+
+// ------------------------------------------------------
+
+// let data = [1, 2, 3, 4, 5, 6, 7]
+// let count = 0
+
+// while (count < data.length) {
+//     console.log(data[count])
+//     count++
+// }
